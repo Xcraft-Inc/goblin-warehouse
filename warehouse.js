5f6d84256d61e7c48cf087d6d63e805b5aaf96bf
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-05-13 17:12:10 +0200

    Use the new Xcraft module declaration

--- warehouse.js
+++ warehouse.js
@@ -1,17 +1,57 @@
 'use strict';
 
-const path = require ('path');
-const service = require ('./lib/service.js');
-
 /**
  * Retrieve the list of available commands.
  *
  * @returns {Object} The list and definitions of commands.
  */
 exports.xcraftCommands = function () {
-  const xUtils = require ('xcraft-core-utils');
   return {
-    handlers: service,
-    rc: xUtils.json.fromFile (path.join (__dirname, './rc.json')),
+    handlers: require ('./lib/service.js'),
+    rc: {
+      upsert: {
+        parallel: true,
+        desc: 'update or insert a new state branch',
+        options: {
+          params: {
+            required: 'branch',
+            optional: 'data',
+          },
+        },
+      },
+      subscribe: {
+        parallel: true,
+        desc: 'create a subscription feed',
+        options: {
+          params: {
+            required: 'feed',
+            optional: 'branches...',
+          },
+        },
+      },
+      unsubscribe: {
+        parallel: true,
+        desc: 'unsubscribe from a feed',
+        options: {
+          params: {
+            required: 'feed',
+          },
+        },
+      },
+      save: {
+        parallel: true,
+        desc: 'persist data warehouse state',
+        options: {
+          params: {},
+        },
+      },
+      load: {
+        parallel: true,
+        desc: 'load persisted data warehouse state',
+        options: {
+          params: {},
+        },
+      },
+    },
   };
 };
